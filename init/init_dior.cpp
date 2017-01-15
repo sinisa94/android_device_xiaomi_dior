e36ba57c8c3af2c1c240c00be11c6e7b2a3ed10f
Ashish Kotnala <ashishkotnala29@users.noreply.github.com>
2017-01-15 21:14:20 +0530

    update build description and fingerprint

--- init/init_dior.cpp
+++ init/init_dior.cpp
@@ -52,6 +52,6 @@ void vendor_load_properties()
 
     property_set("ro.product.device", "dior");
     property_set("ro.build.product", "dior");
-    property_set("ro.build.description", "dior-user 4.4.4 KTU84P V8.1.2.0.KHIMIDI release-keys");
-    property_set("ro.build.fingerprint", "Xiaomi/dior/dior:4.4.4/KTU84P/V8.1.2.0.KHIMIDI:user/release-keys");
+    property_set("ro.build.description", "dior-user 4.4.4 KTU84P V8.1.4.0.KHIMIDI release-keys");
+    property_set("ro.build.fingerprint", "Xiaomi/dior/dior:4.4.4/KTU84P/V8.1.4.0.KHIMIDI:user/release-keys");
 }
