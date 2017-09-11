eb71ead53cafba7346f6cd2e800b1d0fff1b0420
Faheem Kamal <faheem.kamal.a@gmail.com>
2017-09-11 13:12:34 +0600

    camerawrapper: add missing libs for headers

--- camera/Android.mk
+++ camera/Android.mk
@@ -4,12 +4,16 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
+    
+LOCAL_STATIC_LIBRARIES := libbase libarect
 
 LOCAL_C_INCLUDES := \
     system/media/camera/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils
+    libhardware liblog libcamera_client libutils \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.msm8226
